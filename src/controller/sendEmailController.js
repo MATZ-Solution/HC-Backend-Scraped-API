b4ecdc2d55340a6bb93e507dbd27d80d3dd84483
Muhammad Naeem <64369381+MuhammadNaeemShaikh@users.noreply.github.com>
2023-10-17 17:04:23 +0500

    modify email

--- src/controller/sendEmailController.js
+++ src/controller/sendEmailController.js
@@ -12,7 +12,7 @@ const EmailSender = async (req, res, next) => {
     const emailOptions = {
       to,
       subject: 'OTP Verification',
-      text: `Your OTP is: ${otp}`,
+      text: `Your OTP for Best Health Care Service is: ${otp}`,
     };
 
     const emailSent = await sendEmail(emailOptions);
@@ -37,41 +37,41 @@ const EmailSender = async (req, res, next) => {
   }
 };
 
-const emailSend = async (req, res, next) => {
-  try {
-    const { to, subject, text,otp } = req.body;
+// const emailSend = async (req, res, next) => {
+//   try {
+//     const { to, subject, text,otp } = req.body;
 
-    // Generate OTP
-    // const otp = generateOTP();
+//     // Generate OTP
+//     // const otp = generateOTP();
 
-    // Send Email with OTP
-    const emailOptions = {
-      to,
-      subject,
-      text,
-    };
+//     // Send Email with OTP
+//     const emailOptions = {
+//       to,
+//       subject,
+//       text,
+//     };
 
-    const emailSent = await sendEmail(emailOptions);
+//     const emailSent = await sendEmail(emailOptions);
 
-    if (emailSent) {
-      // Save OTP in the database
-      const newOtp = new Otp({
-        email: to,
-        code: otp,
-      });
-      await newOtp.save();
+//     if (emailSent) {
+//       // Save OTP in the database
+//       const newOtp = new Otp({
+//         email: to,
+//         code: otp,
+//       });
+//       await newOtp.save();
 
-      res.status(200).json({
-        success: true,
-        message: 'Email sent with OTP',
-      });
-    } else {
-      throw new Error('Failed to send email');
-    }
-  } catch (err) {
-    next(err);
-  }
-};
+//       res.status(200).json({
+//         success: true,
+//         message: 'Email sent with OTP',
+//       });
+//     } else {
+//       throw new Error('Failed to send email');
+//     }
+//   } catch (err) {
+//     next(err);
+//   }
+// };
 
 const generateOTP = () => {
   const otp = Math.floor(1000 + Math.random() * 9000); // Generates a random 4-digit OTP
@@ -80,5 +80,4 @@ const generateOTP = () => {
 
 module.exports = {
   EmailSender,
-  emailSend,
 };
