866243d70dbf2a2c55181b416ba337b19864ae1f
Maaz-Ur-Rehman <maazurrehman42@gmail.com>
2024-01-04 23:21:14 +0500

    mongo

--- src/utils/db.js
+++ src/utils/db.js
@@ -5,7 +5,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 const connect = () => {
   mongoose
-    .connect(process.env.MONGO_URL, {
+    .connect("mongodb+srv://matzsolutions:2VVG2QxBAMub9Oaz@cluster0.gyal2.mongodb.net/healthcare", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
