8b420366b90c2894c0930681e883095eaa9cc65e
Maaz-Ur-Rehman <maazurrehman42@gmail.com>
2024-04-26 15:12:35 +0500

    add cache

--- src/controller/HealthCareController.js
+++ src/controller/HealthCareController.js
@@ -3337,7 +3337,7 @@ const healthCareController = {
           let query = {};
 
           if (state) {
-            query.state = state;
+            query.state = { $regex: new RegExp(state, 'i') };
           }
 
           if (city) {
